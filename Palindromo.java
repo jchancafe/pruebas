0db6e1783e04cd63663e6af3932f4f09f1e52c5f
Johnny Chancafe <jchancafer@gmail.com>
2024-07-27 08:46:53 -0500

    Update Palindromo.java

--- Palindromo.java
+++ Palindromo.java
@@ -1,18 +1,26 @@
 import java.util.Scanner;
-
+// Para evaluar si un texto es un palindromo
+// ademas se retiran letras hasta tratar que el texto sea un palindromo.
 public class Main {
 	// Para validar si un texto es paligromo
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
+		System.out.println("Ingrese el texto a evaluar");
 		String nombre = entrada.nextLine();
 		String[] posicionLetra = nombre.split("");
+		
+		posicionLetra = obtenerLetrasUnicos(posicionLetra);
+		
+		// va a iterar hasta el texto tenga mas de dos letras.
 		for(int i=0; i< posicionLetra.length; i++) {
+			// 
 			if(esPalindromo(nombre)) {
 				System.out.println("exito: " + nombre);
 				return;
 			}
 			nombre = actualizarTexto(nombre, posicionLetra[i]);
 		}
+		System.out.println("texto final: " + nombre);
 	}
 	
 	private static boolean esPalindromo(String texto) {
@@ -29,6 +37,16 @@ public class Main {
 	
 	private static String actualizarTexto(String texto, String letraEliminar) {
 		return texto.replace(letraEliminar, "");
+	}
+	
+	private static String[] obtenerLetrasUnicos(String[] arrayString) {
+		String nuevoString = "";
+		for(int i=0;i< arrayString.length; i++) {
+			if(!nuevoString.contains(arrayString[i])) {
+				nuevoString += arrayString[i];
+			}
+		}
+		return nuevoString.split("");
 	}
 
 }
